eaee6f730d1a474e7f57cbed985297f0622918a9
Indira Priya Darsini G <95538028+indirapriyadarsini-g@users.noreply.github.com>
2024-03-25 13:26:48 +0530

    Create IHospitalService.java

---
+++ src/com/hexaware/dao/IHospitalService.java
@@ -0,0 +1,16 @@
+package com.hexaware.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.hexaware.entity.Appointment;
+import com.hexaware.exception.PatientNumberNotFoundException;
+
+public interface IHospitalService {
+	Appointment getAppointmentById(int appointmentId) throws SQLException, PatientNumberNotFoundException;
+	List<Appointment> getAppointmentsForPatient(int patientId) throws SQLException, PatientNumberNotFoundException;
+	List<Appointment> getAppointmentsForDoctor(int doctorId) throws SQLException;
+	boolean scheduleAppointment(Appointment appointment) throws SQLException;
+	boolean updateAppointment(Appointment appointment) throws SQLException;
+	boolean cancelAppointment(int appointmentId) throws SQLException;
+}	
